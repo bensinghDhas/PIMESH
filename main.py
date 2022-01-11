ba9ce81072293a714cc36cb1386198694ee48ba2
bensingh <bensingh@iisc.ac.in>
2022-01-11 16:52:41 -0600

    not seamless, need more work

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import matplotlib.pylab as plt
 
 fName="./examples/hole_bdrypts.txt"
 bndryNodes,noSegNodes,noBndryNodes,N_INJ,INJ_DATA, N_REFINEMENTS, REFINEMENT_DATA=util.readFile(fName)
-pm.initializeGeometry(bndryNodes,noSegNodes,noBndryNodes,N_INJ,INJ_DATA,N_REFINEMENTS, REFINEMENT_DATA)
+data=pm.initializeGeometry(bndryNodes,noSegNodes,noBndryNodes,N_INJ,INJ_DATA,N_REFINEMENTS, REFINEMENT_DATA)
+pm.moveNodes(nodes,)
 
 plt.plot(bndryNodes[:,0],bndryNodes[:,1],'o')
 plt.show()
